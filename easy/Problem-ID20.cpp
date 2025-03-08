e61875e38f038b4033a7025a04fe052f515ce736
Amrutha <maddipatiamrutha23@lpu.in>
2025-03-08 20:08:32 +0530

    20. Valid Parantheses

---
+++ easy/Problem-ID20.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        bool isValid(string s) {
+            stack<char>stk;
+            unordered_map<char,char>matching={{')','('},{']','['},{'}','{'}};
+            for(char c:s){
+            if(c=='('||c=='['||c=='{'){
+                stk.push(c);
+            }
+            else{
+                if(stk.empty()||stk.top()!=matching[c]){
+                    return false;
+                }
+                stk.pop();
+            }
+            }
+            return stk.empty();
+        }
+    };
