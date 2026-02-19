c565fc7ba062788bcb762ddb866ef2dc2e5a981b
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-02-19 11:47:07 +0530

    Time: 0 ms (100%), Space: 87 MB (63.27%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,18 +11,43 @@
  */
 class Solution {
 public:
-    vector<int>ans;
-    void pushVec(TreeNode*root){
+ 
+    void inorder(TreeNode*root, vector<int>&ans){
         if(!root)return;
+        if(root->left)inorder(root->left,ans);
         ans.push_back(root->val);
-        if(root->left)pushVec(root->left);
-        if(root->right)pushVec(root->right);
+        if(root->right)inorder(root->right,ans);
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        
-        pushVec(root1);
-        pushVec(root2);
-        sort(ans.begin(),ans.end());
+        vector<int>ans1;
+        vector<int>ans2;
+        inorder(root1,ans1);
+        inorder(root2,ans2);
+        int i=0,j=0,k=0;
+        int n=ans1.size(),m=ans2.size();
+        vector<int>ans(n+m);
+        while(i<n&&j<m){
+            if(ans1[i]<=ans2[j]){
+                ans[k]=ans1[i];
+                k++;
+                i++;
+            }
+            else{
+                ans[k]=ans2[j];
+                k++;
+                j++;
+            }
+        }
+        while(i<n){
+            ans[k]=ans1[i];
+            k++;
+            i++;
+        }
+        while(j<m){
+            ans[k]=ans2[j];
+            k++;
+            j++;
+        }
         return ans;
     }
 };
