022579091a7f3a333835ec7db5a75f9f4d6bd498
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-02-24 11:03:26 +0530

    Time: 9 ms (53.21%), Space: 11.3 MB (81.99%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -6,16 +6,16 @@ public:
         for(char c:t){
             mpp[c]++;
         }
-        int targetRem=t.size();
-        int minWindow[2]={0,INT_MAX};
+        int target=t.size();
         int left=0;
+        int minWindow[2]={0,INT_MAX};
         for(int right=0;right<s.size();right++){
             char c=s[right];
             if(mpp.find(c)!=mpp.end()&&mpp[c]>0){
-                targetRem--;
+                target--;
             }
             mpp[c]--;
-            if(targetRem==0){
+            if(target==0){
                 while(true){
                     char charStart=s[left];
                     if(mpp.find(charStart)!=mpp.end()&&mpp[charStart]==0){
@@ -29,11 +29,10 @@ public:
                     minWindow[1]=right;
                 }
                 mpp[s[left]]++;
-                targetRem++;
+                target++;
                 left++;
             }
-
         }
-        return minWindow[1]>=s.size()?"":s.substr(minWindow[0],minWindow[1]-minWindow[0]+1);
+        return minWindow[1]>s.size()?"":s.substr(minWindow[0],minWindow[1]-minWindow[0]+1);
     }
 };
