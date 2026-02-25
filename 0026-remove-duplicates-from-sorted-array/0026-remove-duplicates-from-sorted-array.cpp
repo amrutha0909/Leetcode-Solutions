22626276cab766feeb0e0369c59808994ec57d9d
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-02-25 21:17:28 +0530

    Time: 3 ms (12.64%), Space: 23.4 MB (5.84%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(mpp.find(nums[i])==mpp.end()){
+                nums[j]=nums[i];
+                j++;
+            }
+            mpp[nums[i]]++;
+        }
+        return j;
+    }
+};
