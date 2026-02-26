a8e17d83835474aaff43316774989ef4bbe82b15
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-02-26 16:17:33 +0530

    Time: 25 ms (45.68%), Space: 85.9 MB (24.45%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    void PSE(vector<int>&heights,vector<int>&pse){
-        stack<int>stk;
-        stk.push(0);
+    void PSE(vector<int>&heights, vector<int>&pse, int n){
+        stack<int>s;
         pse[0]=-1;
-        for(int i=1;i<heights.size();i++){
-            while(!stk.empty()&&heights[stk.top()]>=heights[i]){
-                stk.pop();
+        s.push(0);
+        for(int i=1;i<n;i++){
+            while(!s.empty() && heights[s.top()]>=heights[i]){
+                s.pop();
             }
-            if(stk.empty())pse[i]=-1;
-            else pse[i]=stk.top();
-            stk.push(i);
+            if(!s.empty())pse[i]=s.top();
+            else pse[i]=-1;
+            s.push(i);
         }
     }
-    void NSE(vector<int>&heights,vector<int>&nse){
-        stack<int>stk;
-        int n=heights.size();
-        stk.push(n-1);
+    void NSE(vector<int>&heights,vector<int>&nse, int n){
+        stack<int>s;
         nse[n-1]=n;
+        s.push(n-1);
         for(int i=n-2;i>=0;i--){
-            while(!stk.empty()&&heights[stk.top()]>=heights[i]){
-                stk.pop();
+            while(!s.empty()&&heights[s.top()]>=heights[i]){
+                s.pop();
             }
-            if(stk.empty())nse[i]=n;
-            else nse[i]=stk.top();
-            stk.push(i);
+            if(!s.empty())nse[i]=s.top();
+            else nse[i]=n;
+            s.push(i);
         }
     }
     int largestRectangleArea(vector<int>& heights) {
         int n=heights.size();
         vector<int>pse(n);
         vector<int>nse(n);
-        PSE(heights,pse);
-        NSE(heights,nse);
+        NSE(heights,nse,n);
+        PSE(heights,pse,n);
         int maxArea=INT_MIN;
         for(int i=0;i<n;i++){
-            maxArea=max(maxArea,heights[i]*(nse[i]-pse[i]-1));
+            maxArea=max(maxArea, heights[i]*(nse[i]-pse[i]-1));
         }
         return maxArea;
     }
