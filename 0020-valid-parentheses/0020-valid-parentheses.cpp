09a31652da91f6f9f173e62f75173da3ab5ae80e
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-02-28 23:03:14 +0530

    Time: 0 ms (100%), Space: 9 MB (38.2%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,6 +3,7 @@ public:
     bool isValid(string s) {
         stack<char>st;
         unordered_map<char,char>mpp={{')','('},{']','['},{'}','{'}};
+        if(s.size()==1)return false;
         for(char c:s){
             if(c=='('||c=='['||c=='{'){
                 st.push(c);
@@ -11,6 +12,7 @@ public:
                 if(!st.empty() && mpp[c]==st.top()){
                     st.pop();
                 }
+                else return false;
             }
         }
         return st.empty();
