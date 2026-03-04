40a0505953e359bc59dfd9fc3390824aa640506e
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-04 11:23:20 +0530

    Time: 37 ms (73.31%), Space: 62.6 MB (70.02%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void backtrack(int start,int n, int k, vector<int>&temp, vector<vector<int>>&result){
+        if(temp.size()==k){
+            result.push_back(temp);
+            return;
+        }
+        for(int i=start;i<=n;i++){
+            temp.push_back(i);
+            backtrack(i+1,n,k,temp,result);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>result;
+        vector<int>temp;
+        backtrack(1,n,k,temp,result);
+        return result;
+    }
+};
