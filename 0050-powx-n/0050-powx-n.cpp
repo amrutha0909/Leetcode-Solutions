eb1182672207669559dc816f17d91117f78efba2
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-07 22:36:43 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.44%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double square(double x, long long n){
+        if(n==0)return 1;
+        if(n%2==0)return square(x*x,n/2);
+        else return x*square(x,n-1);
+    }
+    double myPow(double x, int n) {
+        long long N=n;
+        if(n<0)x=1/x,N=N*(-1);
+        return square(x,N);
+    }
+};
