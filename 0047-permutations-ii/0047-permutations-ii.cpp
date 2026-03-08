c28369ecb782c1ee941ce12ecda01f0935402daa
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-08 21:52:19 +0530

    Time: 3 ms (62.91%), Space: 11.3 MB (97.42%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void backtrack(vector<int>&nums,vector<int>&temp, vector<vector<int>>&result, vector<bool>&used){
+    void backtrack(vector<int>&nums, vector<vector<int>>&result, vector<int>&temp, vector<bool>&used){
         if(temp.size()==nums.size()){
             result.push_back(temp);
             return;
         }
         for(int i=0;i<nums.size();i++){
             if(used[i])continue;
-            if(i>0 && nums[i]==nums[i-1] && !used[i-1])continue;
+            if(i>0&&nums[i]==nums[i-1] && !used[i-1])continue;
             used[i]=true;
             temp.push_back(nums[i]);
-            backtrack(nums,temp,result,used);
+            backtrack(nums,result,temp,used);
             temp.pop_back();
             used[i]=false;
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        vector<bool>used(nums.size(),false);
         vector<int>temp;
         vector<vector<int>>result;
-        vector<bool>used(nums.size(),false);
-        backtrack(nums,temp,result,used);
+        sort(nums.begin(),nums.end());
+        backtrack(nums,result,temp,used);
         return result;
     }
 };
