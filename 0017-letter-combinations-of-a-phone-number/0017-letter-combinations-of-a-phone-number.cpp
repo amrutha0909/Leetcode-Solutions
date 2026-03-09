e346cda2742a4a51ccd1a5eeac4545f1a8bbc163
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-09 10:56:10 +0530

    Time: 0 ms (100%), Space: 9.2 MB (75.46%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void backtrack(unordered_map<int,string>&mpp,vector<string>&result,string&digits, int index, string &temp){
+    void backtrack(string&digits,int index,unordered_map<int,string>&mpp,string&current,vector<string>&result){
         if(index==digits.size()){
-            result.push_back(temp);
+            result.push_back(current);
             return;
         }
-        string letters=mpp[digits[index]-'0'];
-        for(char ch:letters){
-            temp.push_back(ch);
-            backtrack(mpp,result,digits,index+1,temp);
-            temp.pop_back();
+        string p=mpp[digits[index]-'0'];
+        for(char c:p){
+            current.push_back(c);
+            backtrack(digits,index+1,mpp,current,result);
+            current.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {};
+        if(digits.empty())return {};
         unordered_map<int,string>mpp={{2,"abc"},{3,"def"},{4,"ghi"},{5,"jkl"},{6,"mno"},{7,"pqrs"},{8,"tuv"},{9,"wxyz"}};
         vector<string>result;
-        string temp;
-        backtrack(mpp,result,digits,0,temp);
+        string current;
+        backtrack(digits,0,mpp,current,result);
         return result;
     }
 };
