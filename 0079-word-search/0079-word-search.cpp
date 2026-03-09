1bd2693a925fccb5ee4a1a5c3f714e57ccbb7024
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-09 10:31:22 +0530

    Time: 809 ms (27.1%), Space: 11.4 MB (46.92%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    bool backtrack(int i, int j, vector<vector<char>>&board, string&word, int index){
+    bool backtrack(int i, int j, int index, vector<vector<char>>&board, string word){
+        if(index==word.size())return true;
         int n=board.size();
         int m=board[0].size();
-        if(index==word.size())return true;
-        if(i>=n||i<0||j>=m||j<0||board[i][j]!=word[index]){
-            return false;
-        }
+        if(i<0||i>=n||j<0||j>=m||board[i][j]!=word[index])return false;
         char temp=board[i][j];
         board[i][j]='#';
-        bool found=backtrack(i+1,j,board,word,index+1)||backtrack(i,j+1,board,word,index+1)||backtrack(i-1,j,board,word,index+1)||backtrack(i,j-1,board,word,index+1);
-        board[i][j]=temp; 
+        bool found=backtrack(i+1,j,index+1,board,word)||backtrack(i,j+1,index+1,board,word)||backtrack(i-1,j,index+1,board,word)||backtrack(i,j-1,index+1,board,word);
+        board[i][j]=temp;
         return found;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-                if(backtrack(i,j,board,word,0))return true;
+        int n=board.size();
+        int m=board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(backtrack(i,j,0,board,word))return true;
             }
         }
         return false;
