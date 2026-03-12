204bb7ca9dc50deecad5d654d1e3c95491d35008
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-12 20:47:52 +0530

    Time: 7 ms (61.18%), Space: 11.5 MB (42.51%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,33 +2,32 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         int left=0;
+        int minWindow[2]={0,INT_MAX};
         int target=t.size();
-        unordered_map<char,int>mpp;
+        unordered_map<int,int>mpp;
         for(char c:t){
             mpp[c]++;
         }
-        int minWindow[2]={0,INT_MAX};
         for(int right=0;right<s.size();right++){
             if(mpp.find(s[right])!=mpp.end() && mpp[s[right]]>0){
                 target--;
             }
             mpp[s[right]]--;
-            if(target==0){
+            if(target==0){                
                 while(true){
-                    char c=s[left];
-                    if(mpp.find(c)!=mpp.end() && mpp[c]==0){
+                    if(mpp.find(s[left])!=mpp.end() && mpp[s[left]]==0){
                         break;
                     }
-                    mpp[c]++;
+                    mpp[s[left]]++;
                     left++;
                 }
-                if(right-left<minWindow[1]-minWindow[0]){
-                    minWindow[0]=left;
+                if(minWindow[1]-minWindow[0]>right-left){
                     minWindow[1]=right;
+                    minWindow[0]=left;
                 }
                 mpp[s[left]]++;
-                target++;
                 left++;
+                target++;
             }
         }
         return minWindow[1]>s.size()?"":s.substr(minWindow[0],minWindow[1]-minWindow[0]+1);
