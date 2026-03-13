920df68c8bc991602c41c9034fcbb360237fb54f
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-13 23:25:43 +0530

    Time: 14 ms (47.49%), Space: 14 MB (22.84%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.cpp
+++ 0126-word-ladder-ii/0126-word-ladder-ii.cpp
@@ -1,60 +1,61 @@
 class Solution {
-    public:
-    unordered_map<string,int>mpp;
-    string b;
-    vector<vector<string>>ans;
-    void dfs(string word, vector<string>&seq){
-        
-        if(word==b){
-            reverse(seq.begin(),seq.end());
-            ans.push_back(seq);
-            reverse(seq.begin(),seq.end());
-            return;
+public:
+    void dfs(string word, string beginWord, unordered_map<string,vector<string>>&parent, vector<string>&path, vector<vector<string>>&res){
+        if(word==beginWord){
+            reverse(path.begin(),path.end());
+            res.push_back(path);
+            reverse(path.begin(),path.end());
         }
-        int steps=mpp[word];
-        for(int i=0;i<word.size();i++){
-            char original=word[i];
-            for(char c='a';c<='z';c++){
-                word[i]=c;
-                if(mpp.find(word)!=mpp.end()&&mpp[word]+1==steps){
-                    seq.push_back(word);
-                    dfs(word,seq);
-                    seq.pop_back();
-                }
-            }
-            word[i]=original;
+        for(string p:parent[word]){
+            path.push_back(p);
+            dfs(p,beginWord,parent,path,res);
+            path.pop_back();
         }
     }
-    
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string>st(wordList.begin(),wordList.end());
+        unordered_set<string>dict(wordList.begin(),wordList.end());
+        vector<vector<string>>res;
+        if(!dict.count(endWord))return res;
+        unordered_map<string,vector<string>>parent;
         queue<string>q;
-        b=beginWord;
         q.push(beginWord);
-        st.erase(beginWord);
-        while(!q.empty()){
-            string word=q.front();
-            int steps=mpp[word];
-            q.pop();
-            if(word==endWord)break;
-            for(int i=0;i<word.size();i++){
-                char original=word[i];
-                for(char c='a';c<='z';c++){
-                    word[i]=c;
-                    if(st.count(word)>0){
-                        q.push(word);
-                        st.erase(word);
-                        mpp[word]=steps+1;
+        unordered_set<string>visited;
+        visited.insert(beginWord);
+        bool found=false;
+        while(!q.empty() && !found){
+            int size=q.size();
+            unordered_set<string>levelVisited;
+            while(size--){
+                string word=q.front();
+                q.pop();
+                string temp=word;
+                for(int i=0;i<temp.size();i++){
+                    char original=temp[i];
+                    for(char c='a';c<='z';c++){
+                        temp[i]=c;
+                        if(dict.count(temp) && !visited.count(temp)){
+                            if(!levelVisited.count(temp)){
+                                q.push(temp);
+                                levelVisited.insert(temp);
+                            }
+                            parent[temp].push_back(word);
+                            if(temp==endWord){
+                                found=true;
+                            }
+                        }
                     }
+                    temp[i]=original;
                 }
-                word[i]=original;
+            }
+            for(string w:levelVisited){
+                visited.insert(w);
             }
         }
-        if(mpp.find(endWord)!=mpp.end()){
-            vector<string>seq;
-            seq.push_back(endWord);
-            dfs(endWord,seq);
+        if(found){
+            vector<string>path;
+            path.push_back(endWord);
+            dfs(endWord,beginWord,parent,path,res);
         }
-        return ans;
+        return res;
     }
 };
