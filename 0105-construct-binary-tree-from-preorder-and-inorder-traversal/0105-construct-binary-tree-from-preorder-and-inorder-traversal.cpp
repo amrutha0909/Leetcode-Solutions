bc116548bafaa645754c9723fa40ad093a379331
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-23 23:21:11 +0530

    Time: 2 ms (79.63%), Space: 27.6 MB (33.77%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,23 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* buildTreeHelper(vector<int>& inorder, vector<int>& preorder, int inorderStart, int inorderEnd, int preorderStart, int preorderEnd, unordered_map<int, int>& index) {
-            if (inorderStart > inorderEnd || preorderStart > preorderEnd) {
-                return nullptr;
-            }
-            int rootVal = preorder[preorderStart];
-            TreeNode* root = new TreeNode(rootVal);
-            int inorderRootIndex = index[rootVal];
-            int leftSubtreeSize = inorderRootIndex - inorderStart;
-            root->left = buildTreeHelper(inorder, preorder, inorderStart, inorderRootIndex - 1, preorderStart+1, preorderStart + leftSubtreeSize, index);
-            root->right = buildTreeHelper(inorder, preorder, inorderRootIndex + 1, inorderEnd, preorderStart + leftSubtreeSize+1, preorderEnd, index);
-            return root;
-        }
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        unordered_map<int, int> index;
-        for (int i = 0; i < inorder.size(); i++) {
-            index[inorder[i]] = i;
-        }
-        return buildTreeHelper(inorder, preorder, 0, inorder.size() - 1, 0, preorder.size() - 1, index);
+    TreeNode*buildHelper(vector<int>&preorder,vector<int>&inorder,int preorderStart,int preorderEnd,int inorderStart,int inorderEnd,unordered_map<int,int>&index){
+        if(inorderStart>inorderEnd||preorderStart>preorderEnd)return nullptr;
+        int rootVal=preorder[preorderStart];
+        TreeNode*root=new TreeNode(rootVal);
+        int inorderRootIndex=index[rootVal];
+        int leftSubtreeSize=inorderRootIndex-inorderStart;
+        root->left=buildHelper(preorder,inorder,preorderStart+1,preorderStart+leftSubtreeSize,inorderStart,inorderRootIndex-1,index);
+        root->right=buildHelper(preorder,inorder,preorderStart+leftSubtreeSize+1,preorderEnd,inorderRootIndex+1,inorderEnd,index);
+        return root;
     }
-    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int,int>index;
+        for(int i=0;i<inorder.size();i++){
+            index[inorder[i]]=i;
+        }
+        return buildHelper(preorder,inorder,0,preorder.size()-1,0,inorder.size()-1,index);
+    }
 };
