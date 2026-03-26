b83287ea6c7ca3f08a934fe996c7a7273eae06fe
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-26 17:43:52 +0530

    Time: 4 ms (14.62%), Space: 16.5 MB (15.41%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int helper(int i, int j, vector<vector<int>>& grid,vector<vector<int>>& dp){
+    int helper(int i, int j,vector<vector<int>>&dp,vector<vector<int>>&grid){
         int n=grid.size();
         int m=grid[0].size();
-        if(i>=n||j>=m)return 1e9;
-        if(i==n-1&&j==m-1)return grid[n-1][m-1];
+        if(i==n-1&&j==m-1){
+            return grid[i][j];
+        }
+        if(i>=n||j>=m)return 1e9; //if you return INT_MAX it will signed integer overflow
         if(dp[i][j]!=-1)return dp[i][j];
-        int way1=grid[i][j]+helper(i+1,j,grid,dp);
-        int way2=grid[i][j]+helper(i,j+1,grid,dp);
+        int way1=grid[i][j]+helper(i,j+1,dp,grid);
+        int way2=grid[i][j]+helper(i+1,j,dp,grid);
         return dp[i][j]=min(way1,way2);
     }
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return helper(0,0,grid,dp);
+        return helper(0,0,dp,grid);
     }
 };
