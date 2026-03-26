1c39dbe6a4ff9cdb014a3e55df14d888fe6b7b61
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-26 17:48:48 +0530

    Time: 2 ms (34.7%), Space: 16.4 MB (39.79%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-    int helper(int i, int j,vector<vector<int>>&dp,vector<vector<int>>&grid){
-        int n=grid.size();
-        int m=grid[0].size();
-        if(i==n-1&&j==m-1){
-            return grid[i][j];
-        }
-        if(i>=n||j>=m)return 1e9; //if you return INT_MAX it will signed integer overflow
-        if(dp[i][j]!=-1)return dp[i][j];
-        int way1=grid[i][j]+helper(i,j+1,dp,grid);
-        int way2=grid[i][j]+helper(i+1,j,dp,grid);
-        return dp[i][j]=min(way1,way2);
-    }
+    // int helper(int i, int j,vector<vector<int>>&dp,vector<vector<int>>&grid){
+    //     int n=grid.size();
+    //     int m=grid[0].size();
+    //     if(i==n-1&&j==m-1){
+    //         return grid[i][j];
+    //     }
+    //     if(i>=n||j>=m)return 1e9; //if you return INT_MAX it will signed integer overflow
+    //     if(dp[i][j]!=-1)return dp[i][j];
+    //     int way1=grid[i][j]+helper(i,j+1,dp,grid);
+    //     int way2=grid[i][j]+helper(i+1,j,dp,grid);
+    //     return dp[i][j]=min(way1,way2);
+    // }
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return helper(0,0,dp,grid);
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        //return helper(0,0,dp,grid);
+        dp[0][0]=grid[0][0];
+        for(int i=1;i<n;i++){
+            dp[i][0]=dp[i-1][0]+grid[i][0];
+        }
+        for(int j=1;j<m;j++){
+            dp[0][j]=dp[0][j-1]+grid[0][j];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
