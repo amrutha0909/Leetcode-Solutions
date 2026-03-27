1848a263ab26f9cf668cc1098c5b0a2453b7edf0
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-27 12:32:27 +0530

    Time: 0 ms (100%), Space: 31.4 MB (49.69%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -4,21 +4,11 @@ public:
         int n=mat.size();
         int m=mat[0].size();
         int r=k%m;
-        vector<vector<int>>original=mat;
-        vector<vector<int>>newMat(n,vector<int>(m));
-        for(int i=0;i<r;i++){
-            for(int x=0;x<n;x++){
-                for(int y=0;y<m;y++){
-                    newMat[x][y]=mat[x][(y+1)%m];
-                }
-            }
-            mat=newMat;
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(original[i][j]!=mat[i][j])return false;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(mat[i][j] != mat[i][(j + r) % m]) return false;
             }
         }
-        return true;
+        return true;;
     }
 };
