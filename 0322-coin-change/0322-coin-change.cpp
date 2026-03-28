2291fa67e7eae04db8c93089a2f57db5f3cfadc8
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-28 23:35:53 +0530

    Time: 59 ms (14.94%), Space: 49.5 MB (26.47%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    const int INF = 1e9;
-    int helper(int index, int amount, vector<int>&coins,vector<vector<int>>&dp){
+    int helper(int index,vector<int>&coins,int amount,vector<vector<int>>&dp){
         if(index==coins.size()-1){
             if(amount%coins[index]==0){
                 return amount/coins[index];
             }
-            else return INF;
+            else return 1e9;
         }
+        if(index>=coins.size())return 1e9;
         if(dp[index][amount]!=-1)return dp[index][amount];
-        int not_take=helper(index+1,amount,coins,dp);
+        int not_take=helper(index+1,coins,amount,dp);
         int take=INT_MAX;
         if(coins[index]<=amount){
-            take=1+helper(index,amount-coins[index],coins,dp);
+            take=1+helper(index,coins,amount-coins[index],dp);
         }
         return dp[index][amount]=min(take,not_take);
     }
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-        int ans = helper(0, amount, coins, dp);
-        return (ans >= INF) ? -1 : ans;
+        int ans=helper(0,coins,amount,dp);
+        return (ans>=1e9)?-1:ans;
+
     }
 };
