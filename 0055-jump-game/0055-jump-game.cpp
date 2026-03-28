3074d57bc025c19df56700340aeb124c160f01dc
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-28 16:33:44 +0530

    Time: 1987 ms (5.01%), Space: 55.8 MB (6.23%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxReach=0;
-        for(int i=0;i<nums.size();i++){
-            if(i>maxReach)return false;
-            maxReach=max(maxReach,i+nums[i]);
+        queue<int>q;
+        int n=nums.size();
+        vector<bool>visited(n,false);
+        q.push(0);
+        visited[0]=true;
+        while(!q.empty()){
+            int s=q.size();
+            while(s--){
+                auto node=q.front();
+                q.pop();
+                int steps=nums[node];
+                if(node==n-1)return true;
+                for(int i=1;i<=steps;i++){
+                    if(node+i<n&&!visited[node+i]){
+                        visited[node+i]=true;
+                        q.push(node+i);
+                    }
+                }
+            }
         }
-        return true;
+        return false;
     }   
 };
