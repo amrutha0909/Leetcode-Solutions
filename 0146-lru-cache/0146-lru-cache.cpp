8cea0f8d4112ac5892a9737feda22496d657e7db
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-30 19:11:52 +0530

    Time: 96 ms (36.87%), Space: 183.6 MB (23.13%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -3,13 +3,14 @@ public:
     class Node{
         public:
         int key;
-        int value;
+        int val;
         Node*next;
         Node*prev;
-        Node(int k, int v){
+        Node(int k,int v){
             key=k;
-            value=v;
-            prev=next=nullptr;
+            val=v;
+            next=nullptr;
+            prev=nullptr;
         }
     };
     unordered_map<int,Node*>mpp;
@@ -23,14 +24,6 @@ public:
         head->next=tail;
         tail->prev=head;
     }
-
-    void remove(Node*node){
-        Node*prevNode=node->prev;
-        Node*nextNode=node->next;
-        prevNode->next=nextNode;
-        nextNode->prev=prevNode;
-    }
-
     void insert(Node*node){
         Node*nextNode=head->next;
         head->next=node;
@@ -38,29 +31,34 @@ public:
         node->next=nextNode;
         nextNode->prev=node;
     }
-
+    void remove(Node*node){
+        Node*prevNode=node->prev;
+        Node*nextNode=node->next;
+        prevNode->next=nextNode;
+        nextNode->prev=prevNode;
+    }
     int get(int key) {
         if(mpp.find(key)==mpp.end())return -1;
         Node*node=mpp[key];
         remove(node);
         insert(node);
-        return node->value;
+        return node->val;
     }
     
     void put(int key, int value) {
+        Node*node=new Node(key,value);
         if(mpp.find(key)!=mpp.end()){
             Node*existing=mpp[key];
             remove(existing);
             mpp.erase(key);
         }
-        if(mpp.size()==capacity){
+        if(capacity==mpp.size()){
             Node*lru=tail->prev;
             remove(lru);
             mpp.erase(lru->key);
         }
-        Node*newNode=new Node(key,value);
-        insert(newNode);
-        mpp[key]=newNode;
+        insert(node);
+        mpp[key]=node;
     }
 };
 
