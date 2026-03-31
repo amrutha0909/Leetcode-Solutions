5e32151e9202a6c3c11f3c68cb8ea164d7aa606f
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-31 16:34:30 +0530

    Time: 0 ms (100%), Space: 46.3 MB (32.68%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
-        int n=cardPoints.size();
-        int sum=0;
-        int win=n-k;
-        int left=0;
-        int minSum=INT_MAX;
         int total=0;
+        int n=cardPoints.size();
         for(int num:cardPoints){
             total+=num;
         }
+        int win=n-k; //picking k cards from the ends means there are n-k cards left in the middle. To maximize the sum of the k picked cards, we have to minimize the sum of the n-k unpicked cards
+        int left=0;
+        int sum=0;
+        int minSum=INT_MAX;
         if(n==k)return total;
         for(int right=0;right<n;right++){
             sum+=cardPoints[right];
-            while(right-left+1>win){
+            if(right-left+1>win){
                 sum-=cardPoints[left];
                 left++;
             }
