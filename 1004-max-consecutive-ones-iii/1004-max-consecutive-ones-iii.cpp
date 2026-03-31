52c96cca41bb0309718096e7be89a22584b08d2d
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-03-31 16:59:32 +0530

    Time: 8 ms (6.19%), Space: 69.7 MB (26.38%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
+        int zeroCount=0;
         int maxLen=0;
-        int maxFreq=0;
-        unordered_map<int,int>mpp;
         int left=0;
         for(int right=0;right<nums.size();right++){
-            if(nums[right]==1)mpp[nums[right]]++;
-            maxFreq=max(maxFreq,mpp[nums[right]]);
-            while(right-left+1-maxFreq>k){
-                mpp[nums[left]]--;
-                if(mpp[nums[left]]==0)mpp.erase(nums[left]);
+            if(nums[right]==0)zeroCount++;
+            if(zeroCount>k){
+                if(nums[left]==0)zeroCount--;
                 left++;
             }
             maxLen=max(maxLen,right-left+1);
