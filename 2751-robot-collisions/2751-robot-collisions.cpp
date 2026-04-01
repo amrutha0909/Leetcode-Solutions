0fa8a17e67f33b4c5d6cc5f3bf66f7deb766c970
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-01 18:49:40 +0530

    Time: 258 ms (12.97%), Space: 267.4 MB (9.58%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -8,10 +8,10 @@ public:
             d_mpp[positions[i]]=directions[i];
         }
         vector<int>ori=positions;
-        unordered_map<int,int>original;
-        for(int i=0;i<positions.size();i++){
-            original[positions[i]]=i;
-        }
+        //unordered_map<int,int>original;
+        // for(int i=0;i<positions.size();i++){
+        //     original[positions[i]]=i;
+        // }
         sort(positions.begin(),positions.end());
         stack<int>st;
         for(int i=0;i<positions.size();i++){
@@ -22,12 +22,12 @@ public:
                 while(!st.empty()&&d_mpp[positions[st.top()]]=='R'){
                     if(h_mpp[positions[st.top()]]<h_mpp[positions[i]]){
                         h_mpp[positions[i]]--;
-                        original.erase(positions[st.top()]);
+                        //original.erase(positions[st.top()]);
                         h_mpp.erase(positions[st.top()]);
                         st.pop();
                     }
                     else if(h_mpp[positions[st.top()]]>h_mpp[positions[i]]){
-                        original.erase(positions[i]);
+                        //original.erase(positions[i]);
                         h_mpp.erase(positions[i]);
                         h_mpp[positions[st.top()]]--;
                         break;
@@ -35,8 +35,8 @@ public:
                     else{
                         h_mpp.erase(positions[st.top()]);
                         h_mpp.erase(positions[i]);
-                        original.erase(positions[st.top()]);
-                        original.erase(positions[i]);
+                        //original.erase(positions[st.top()]);
+                        //original.erase(positions[i]);
                         st.pop();
                         break;
                     }
@@ -45,7 +45,7 @@ public:
         }
         vector<int>ans;
         for(int i=0;i<ori.size();i++){
-            if(original.find(ori[i])!=original.end()){
+            if(h_mpp.find(ori[i])!=h_mpp.end()){
                 ans.push_back(h_mpp[ori[i]]);
             }
         }     
