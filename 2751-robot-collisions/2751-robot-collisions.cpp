c6ed1dab40781007c65cf70485d5a701ddc0753e
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-01 18:50:34 +0530

    Time: 256 ms (12.97%), Space: 267.2 MB (9.78%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -8,10 +8,7 @@ public:
             d_mpp[positions[i]]=directions[i];
         }
         vector<int>ori=positions;
-        //unordered_map<int,int>original;
-        // for(int i=0;i<positions.size();i++){
-        //     original[positions[i]]=i;
-        // }
+
         sort(positions.begin(),positions.end());
         stack<int>st;
         for(int i=0;i<positions.size();i++){
@@ -22,12 +19,10 @@ public:
                 while(!st.empty()&&d_mpp[positions[st.top()]]=='R'){
                     if(h_mpp[positions[st.top()]]<h_mpp[positions[i]]){
                         h_mpp[positions[i]]--;
-                        //original.erase(positions[st.top()]);
                         h_mpp.erase(positions[st.top()]);
                         st.pop();
                     }
                     else if(h_mpp[positions[st.top()]]>h_mpp[positions[i]]){
-                        //original.erase(positions[i]);
                         h_mpp.erase(positions[i]);
                         h_mpp[positions[st.top()]]--;
                         break;
@@ -35,8 +30,6 @@ public:
                     else{
                         h_mpp.erase(positions[st.top()]);
                         h_mpp.erase(positions[i]);
-                        //original.erase(positions[st.top()]);
-                        //original.erase(positions[i]);
                         st.pop();
                         break;
                     }
