8408aa6c211c4be2061513772a089e10d9db255c
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-01 15:05:34 +0530

    Time: 369 ms (6.39%), Space: 302.1 MB (6.19%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -1,78 +1,54 @@
 class Solution {
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        
-        unordered_map<int,int> h_mpp;   // position -> health
-        unordered_map<int,char> d_mpp;  // position -> direction
-        
-        int n = positions.size();
-        
-        for(int i = 0; i < n; i++){
-            h_mpp[positions[i]] = healths[i];
-            d_mpp[positions[i]] = directions[i];
+        unordered_map<int,int>h_mpp;
+        unordered_map<int,char>d_mpp;
+        for(int i=0;i<positions.size();i++){
+            h_mpp[positions[i]]=healths[i];
+            d_mpp[positions[i]]=directions[i];
         }
-
-        vector<int> ori = positions;
-
-        unordered_map<int,int> original; // track alive
-        for(int i = 0; i < n; i++){
-            original[positions[i]] = i;
+        vector<int>ori=positions;
+        unordered_map<int,int>original;
+        for(int i=0;i<positions.size();i++){
+            original[positions[i]]=i;
         }
-
-        sort(positions.begin(), positions.end());
-
-        stack<int> st; // stores indices of sorted positions
-
-        for(int i = 0; i < n; i++){
-            
-            int currPos = positions[i];
-
-            // ✅ only push RIGHT moving robots
-            if(d_mpp[currPos] == 'R'){
+        sort(positions.begin(),positions.end());
+        stack<int>st;
+        for(int i=0;i<positions.size();i++){
+            if(d_mpp[positions[i]]=='R'){
                 st.push(i);
             }
-            else{ // 'L'
-                
-                // ✅ handle multiple collisions
-                while(!st.empty() && d_mpp[positions[st.top()]] == 'R'){
-                    
-                    int topPos = positions[st.top()];
-
-                    if(h_mpp[topPos] < h_mpp[currPos]){
-                        // top robot dies
-                        h_mpp[currPos]--;
-                        original.erase(topPos);
-                        h_mpp.erase(topPos);
+            else{
+                while(!st.empty()&&d_mpp[positions[st.top()]]=='R'){
+                    if(h_mpp[positions[st.top()]]<h_mpp[positions[i]]){
+                        h_mpp[positions[i]]--;
+                        original.erase(positions[st.top()]);
+                        h_mpp.erase(positions[st.top()]);
                         st.pop();
                     }
-                    else if(h_mpp[topPos] > h_mpp[currPos]){
-                        // current robot dies
-                        h_mpp[topPos]--;
-                        original.erase(currPos);
-                        h_mpp.erase(currPos);
+                    else if(h_mpp[positions[st.top()]]>h_mpp[positions[i]]){
+                        original.erase(positions[i]);
+                        h_mpp.erase(positions[i]);
+                        h_mpp[positions[st.top()]]--;
                         break;
                     }
                     else{
-                        // both die
-                        original.erase(topPos);
-                        original.erase(currPos);
-                        h_mpp.erase(topPos);
-                        h_mpp.erase(currPos);
+                        h_mpp.erase(positions[st.top()]);
+                        h_mpp.erase(positions[i]);
+                        original.erase(positions[st.top()]);
+                        original.erase(positions[i]);
                         st.pop();
                         break;
                     }
                 }
             }
         }
-
-        vector<int> ans;
-
-        for(int i = 0; i < ori.size(); i++){
-            if(original.find(ori[i]) != original.end()){
+        vector<int>ans;
+        for(int i=0;i<ori.size();i++){
+            if(original.find(ori[i])!=original.end()){
                 ans.push_back(h_mpp[ori[i]]);
             }
-        }
-
+        }     
         return ans;
     }
 };
