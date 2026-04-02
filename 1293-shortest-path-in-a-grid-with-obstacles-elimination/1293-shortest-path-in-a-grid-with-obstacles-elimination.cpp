b724df984f56950ef4fdbb89880c3300c7c3cd19
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-02 19:14:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -5,27 +5,29 @@ public:
         int m=grid[0].size();
         vector<vector<vector<bool>>>visited(n,vector<vector<bool>>(m,vector<bool>(k+1,false)));
         queue<vector<int>>q;
-        q.push({0,0,k,0});
+        q.push({0,k,0,0});
         while(!q.empty()){
-            auto it=q.front();
-            q.pop();
-            int x=it[0];
-            int y=it[1];
-            int stops=it[2];
-            int dist=it[3];
-            vector<int>dX={-1,0,1,0};
-            vector<int>dY={0,1,0,-1};
-            visited[x][y][stops]=true;
-            if(x==n-1&&y==m-1)return dist;
-            for(int i=0;i<4;i++){
-                int nx=x+dX[i];
-                int ny=y+dY[i];
-                if(nx>=0&&nx<n&&ny>=0&&ny<m){
-                    int curr_stops=grid[nx][ny]==1?1:0;
-                    int next_k=stops-curr_stops;
-                    if(next_k>=0&&!visited[nx][ny][next_k]){
-                        visited[nx][ny][stops-curr_stops]=true;
-                        q.push({nx,ny,next_k,dist+1});
+            int len=q.size();
+            for(int i=0;i<len;i++){
+                auto it=q.front();
+                q.pop();
+                int dist=it[0];
+                int obs=it[1];
+                int x=it[2];
+                int y=it[3];
+                vector<int>dX={-1,0,1,0};
+                vector<int>dY={0,1,0,-1};
+                if(x==n-1&&y==m-1)return dist;
+                visited[x][y][obs]=true;
+                for(int i=0;i<4;i++){
+                    int nx=x+dX[i];
+                    int ny=y+dY[i];
+                    if(nx>=0 && nx<n && ny>=0 && ny<m){
+                        int curr_obs=grid[nx][ny]==1?1:0;
+                        int next_k=obs-curr_obs;
+                        if(next_k>=0&&!visited[nx][ny][next_k]){
+                            q.push({dist+1,next_k,nx,ny});
+                        }
                     }
                 }
             }
