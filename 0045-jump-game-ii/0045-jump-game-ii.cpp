a372b9c90ea3c4e6c06c2b4a4f570a4bbd429bcc
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-02 23:14:19 +0530

    Time: 1 ms (30.01%), Space: 20.5 MB (32.84%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        queue<int>q;
-        q.push(0);
-        int ans=0;
-        vector<bool>visited(nums.size(),false);
-        while(!q.empty()){
-            int len=q.size();
-            for(int i=0;i<len;i++){
-                int index=q.front();
-                q.pop();
-                if(index==nums.size()-1)return ans;
-                visited[index]=true;
-                int steps=nums[index];
-                for(int i=index;i<=index+steps;i++){
-                    if(i<nums.size()&&!visited[i]){
-                        visited[i]=true;
-                        q.push(i);
-                    }
-                }
+    int jump(vector<int>& nums) {   
+        int currEnd=0;
+        int farthest=0;
+        int jumps=0;
+        for(int i=0;i<nums.size()-1;i++){
+            farthest=max(farthest,i+nums[i]);
+            if(i==currEnd){
+                currEnd=farthest;
+                jumps++;
             }
-            ans++;
         }
-        return ans;
+        return jumps;
     }
 };
