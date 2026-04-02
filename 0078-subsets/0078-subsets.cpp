62b669b1991d83a8f6d44d65495f42efac1daa10
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-02 22:46:59 +0530

    Time: 0 ms (100%), Space: 10 MB (58.94%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     void backtrack(int start,vector<vector<int>>&res,vector<int>&nums,vector<int>&temp){
         res.push_back(temp);
-        if(temp.size()==nums.size()){
-            return;
-        }
         for(int i=start;i<nums.size();i++){
-            if(temp.empty()||temp.back()!=nums[i])temp.push_back(nums[i]);
+            temp.push_back(nums[i]);
             backtrack(i+1,res,nums,temp);
             temp.pop_back();
         }
