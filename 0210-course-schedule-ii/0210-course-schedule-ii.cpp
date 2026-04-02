d56b36866f095a39ad3a64d78c18514725dcfea6
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-02 20:45:04 +0530

    Time: 3 ms (72.46%), Space: 19.3 MB (13.9%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    vector<int>res;
     stack<int>stk;
     bool dfs(int node,vector<vector<int>>&adj,vector<bool>&visited,vector<bool>&pathVis){
         visited[node]=true;
         pathVis[node]=true;
-        
-        for(auto&it:adj[node]){
+        for(auto it:adj[node]){
             if(!visited[it]){
                 if(!dfs(it,adj,visited,pathVis))return false;
             }
-            else if(pathVis[it])return false;
+            else {
+                if(pathVis[it])return false;
+            }
         }
-        pathVis[node]=false;
         stk.push(node);
+        pathVis[node]=false;
         return true;
     }
-    bool topoSort(int numCourses,vector<vector<int>>&adj,vector<bool>&visited,vector<bool>&pathVis){
+    bool topologicalSort(int numCourses,vector<vector<int>>&adj,vector<bool>&visited,vector<bool>&pathVis,vector<int>&res){
         for(int i=0;i<numCourses;i++){
             if(!visited[i]){
                 if(!dfs(i,adj,visited,pathVis))return false;
@@ -29,16 +29,21 @@ public:
         }
         return true;
     }
-    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
-        for(auto&edge:pre){
+        for(auto edge:prerequisites){
             int u=edge[0];
             int v=edge[1];
             adj[v].push_back(u);
         }
         vector<bool>visited(numCourses,false);
         vector<bool>pathVis(numCourses,false);
-        if(!topoSort(numCourses,adj,visited,pathVis))return {};
+        vector<int>res;
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                if(!topologicalSort(numCourses,adj,visited,pathVis,res))return {};
+            }
+        }
         return res;
     }
 };
