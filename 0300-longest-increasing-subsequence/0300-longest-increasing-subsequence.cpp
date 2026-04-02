9d11d9a617753f9e51a0f367a7591002068eeb22
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-02 19:44:27 +0530

    Time: 0 ms (100%), Space: 14.2 MB (68.33%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> sub; // stores the "smallest tail" of increasing subsequences
-
-        for (int num : nums) {
-            int i = 0, j = sub.size() - 1;
-            int pos = sub.size(); // default: append to sub if num is bigger than all
-
-            // Simple binary search to find first element >= num
-            while (i <= j) {
-                int mid = (i + j) / 2;
-                if (sub[mid] >= num) {
-                    pos = mid;   // found a candidate to replace
-                    j = mid - 1;
-                } else {
-                    i = mid + 1;
+        vector<int>result;
+        for(int num:nums){
+            int left=0;
+            int right=result.size()-1;
+            while(left<=right){
+                int mid=left+(right-left)/2;
+                if(num>result[mid]){
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
                 }
             }
-
-            if (pos == sub.size()) {
-                // num is bigger than all → append
-                sub.push_back(num);
-            } else {
-                // replace to keep subsequence "tail" small
-                sub[pos] = num;
-            }
+            if(left==result.size())result.push_back(num);
+            else result[left]=num;
         }
-
-        return sub.size(); // length of LIS
+        return result.size();
     }
 };
