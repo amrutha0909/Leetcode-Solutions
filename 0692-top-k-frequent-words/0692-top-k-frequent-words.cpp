4a70fed599dd1f896bb698f8aeaeddd6de2c80b5
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-03 12:52:15 +0530

    Time: 1 ms (76.61%), Space: 17.9 MB (66.58%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string,int>mpp;
+        for(string word:words)mpp[word]++;
+        vector<vector<string>> bucket(words.size() + 1);
+        for(auto&it:mpp){
+            bucket[it.second].push_back(it.first);
+        }
+        for(auto&x:bucket){
+            sort(x.begin(),x.end());
+        }
+        vector<string>ans;
+        int i=bucket.size()-1;
+        for(int i = bucket.size() - 1; i >= 0 && k > 0; i--){
+            for(string &word : bucket[i]){
+                ans.push_back(word);
+                k--;
+                if(k == 0) break;
+            }
+        }
+        return ans;
+    }
+};
