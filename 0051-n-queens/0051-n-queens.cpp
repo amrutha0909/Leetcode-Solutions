36eb81eff4fcf7eb22812c9f1cd44b48d4c5b827
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-04 19:13:24 +0530

    Time: 0 ms (100%), Space: 10.2 MB (54.62%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    bool couldPlace(int row, int col, vector<string>&board,int n){
+    bool isValid(int row,int col,vector<string>&board,int n){
         for(int i=0;i<n;i++){
             if(board[i][col]=='Q')return false;
         }
         for(int j=0;j<n;j++){
             if(board[row][j]=='Q')return false;
-        }
+        }   
         for(int i=row,j=col;i>=0&&j>=0;i--,j--){
             if(board[i][j]=='Q')return false;
         }
@@ -15,23 +15,23 @@ public:
         }
         return true;
     }
-    void backtrack(vector<string>&board,vector<vector<string>>&ans,int row,int n){
+    void backtrack(int row,int n,vector<string>&board,vector<vector<string>>&ans){
         if(row==n){
             ans.push_back(board);
             return;
         }
-        for(int j=0;j<n;j++){
-            if(couldPlace(row,j,board,n)){
-                board[row][j]='Q';
-                backtrack(board,ans,row+1,n);
-                board[row][j]='.';
+        for(int col=0;col<n;col++){
+            if(isValid(row,col,board,n)){
+                board[row][col]='Q';
+                backtrack(row+1,n,board,ans);
+                board[row][col]='.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<string>board(n,string(n,'.'));
         vector<vector<string>>ans;
-        backtrack(board,ans,0,n);
+        backtrack(0,n,board,ans);
         return ans;
     }
 };
