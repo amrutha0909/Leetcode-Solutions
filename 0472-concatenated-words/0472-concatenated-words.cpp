6b4c1918de886c2a7b3dbe07be8e37701f1edf98
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-05 18:20:52 +0530

    Time: 125 ms (69.77%), Space: 49.2 MB (94.18%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        unordered_set<string>dictionary(words.begin(),words.end());
-        vector<string>answer;
+        unordered_set<string>st(words.begin(),words.end());
+        vector<string>res;
         for(const string&word:words){
-            const int length=word.length();
-            vector<bool>dp(length+1);
+            int length=word.size();
+            vector<bool>dp(length+1,false);
             dp[0]=true;
             for(int i=1;i<=length;i++){
-                for(int j=(i==length?1:0);!dp[i]&&j<i;j++){
-                    dp[i]=dp[j]&&dictionary.count(word.substr(j,i-j));
+                for(int j=(i==length)?1:0;!dp[i]&&j<i;j++){
+                    dp[i]=dp[j]&&st.count(word.substr(j,i-j));
                 }
             }
-            if(dp[length]){
-                answer.push_back(word);
-            }
+            if(dp[length])res.push_back(word);
         }
-        return answer;
+        return res;
     }
 };
