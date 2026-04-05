ceb2495c9d99408d023037f1401bb6c96bc404af
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-05 17:28:10 +0530

    Time: 339 ms (52.34%), Space: 19 MB (78.92%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,66 +1,66 @@
 class Solution {
 public:
-    // Approach : Collect the initial of each word and the word itself in a set.
-    // also for each initial collect the positions, so as to dfs only those locations
-    // use set.lower_bound() inside dfs to find if curr string exist in the set,
-    // if yes then push into ans,
-    // backtrack when curr.size()>=largest string size starting with current intial 
-    // use a viisited array to keep track of visited posistions 
-    // during the dfs to rule out possibility of repeated words.
-    // also keep the ans into a set so that two positions with same
-    // initial letters donot give the same word.
-
-    int dir[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
-    void dfs(vector<vector<char>>& b,set<string>&st,int r,int c,string&curr,set<string>&ans,int lsz,vector<vector<int>>&vis){
-        auto it=st.lower_bound(curr);
-        if (it!=st.end()&&*it==curr)ans.insert(curr);
-        if((int)curr.size()==lsz)return;
-        vis[r][c] = 1;
-        for (int k = 0; k < 4; k++){
-            int nr = r + dir[k][0];
-            int nc = c + dir[k][1];
-            if (nr>=0&&nc>=0&&nr<b.size()&&nc<b[0].size()&&!vis[nr][nc]){
-                curr.push_back(b[nr][nc]);
-                dfs(b,st,nr,nc,curr,ans,lsz,vis);
-                curr.pop_back();
-            }
+    vector<string>result;
+    int m,n;
+    struct Trie{
+        bool isLeaf;
+        Trie*children[26];
+        string word;
+    };
+    Trie*getNode(){
+        Trie*node=new Trie();
+        node->isLeaf=false;
+        node->word="";
+        for(int i=0;i<26;i++){
+            node->children[i]=nullptr;
         }
-        vis[r][c] = 0; // backtrack
+        return node;
     }
-
-    vector<string> findWords(vector<vector<char>>& b, vector<string>& words) {        
-        int l=b.size();
-        int k=b[0].size();
-        unordered_map<char,set<string>>mpp;
-        for(auto w:words)mpp[w[0]].insert(w);
-        // for(auto [w,st]:mpp){
-        //     for(auto i:st)cout<<i<<" ";
-        //     cout<<endl;
-        // }
-        unordered_map<char,vector<pair<int,int>>>pos;
-        for(int i = 0;i < b.size();i++){
-            for(int j = 0;j < b[0].size();j++){
-                char ch = b[i][j];
-                pos[ch].push_back({i,j});
+    void insert(Trie*root,string word){
+        Trie*node=root;
+        for(char c:word){
+            if(node->children[c-'a']==nullptr){
+                Trie*newNode=new Trie();
+                node->children[c-'a']=newNode;
+            }
+            node=node->children[c-'a'];
+        }
+        node->isLeaf=true;
+        node->word=word;
+    }
+    vector<int>dX={-1,0,1,0};
+    vector<int>dY={0,1,0,-1};
+    void dfs(int i,int j,vector<vector<char>>&board,Trie*root){
+        if(i<0||i>=n||j<0||j>=m)return;
+        if(board[i][j]=='$'||root->children[board[i][j]-'a']==nullptr)return;
+        root=root->children[board[i][j]-'a'];
+        if(root->isLeaf){
+            result.push_back(root->word);
+            root->isLeaf=false;
+        }
+        char temp=board[i][j];
+        board[i][j]='$';
+        for(int k=0;k<4;k++){
+            int nx=i+dX[k];
+            int ny=j+dY[k];
+            dfs(nx,ny,board,root);
+        }
+        board[i][j]=temp;
+    }
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) { 
+        Trie*root=getNode();
+        for(string word:words){
+            insert(root,word);
+        }  
+        n=board.size(),m=board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                char ch=board[i][j];
+                if(root->children[ch-'a']!=nullptr){
+                    dfs(i,j,board,root);
+                }
             }
         }
-        set<string>ans;
-        for(auto m:mpp){
-            char start = m.first;
-            set<string>&st = m.second;
-            int lsz = 0; // find longest word length for this starting char
-            for (auto &w : st) lsz = max(lsz, (int)w.size());
-            // cout<<lsz;
-            for(auto p:pos[start]){
-                int i = p.first;
-                int j = p.second;
-                vector<vector<int>>vis(l,vector<int>(k,0));
-                string curr(1, b[i][j]);
-                // cout<<curr<<endl;
-                dfs(b,st,i,j,curr,ans,lsz,vis);
-            }
-        }
-        vector<string>v(ans.begin(),ans.end());
-        return v;
+        return result;
     }
 };
