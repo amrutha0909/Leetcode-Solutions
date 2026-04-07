e3363719960ba052d6b1c3935da5017ab94922cc
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-07 15:27:48 +0530

    Time: 2 ms (17.02%), Space: 18.4 MB (31.58%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        int col=image[sr][sc];
-        int n=image.size();
-        int m=image[0].size();
+    int n,m;
+    void dfs(int sr,int sc,int color, int original,vector<vector<int>>&image,vector<vector<bool>>&visited){
         image[sr][sc]=color;
-        vector<vector<bool>>vis(n,vector<bool>(m,false));
-        vis[sr][sc]=true;
-        while(!q.empty()){
-            auto it=q.front();
-            q.pop();
-            int x=it.first;
-            int y=it.second;
-            vector<int>dX={-1,0,1,0};
-            vector<int>dY={0,1,0,-1};
-            for(int i=0;i<4;i++){
-                int nx=x+dX[i];
-                int ny=y+dY[i];
-                if(nx>=0&&nx<n&&ny>=0&&ny<m&&image[nx][ny]==col&&!vis[nx][ny]){
-                    vis[nx][ny]=true;
-                    image[nx][ny]=color;
-                    q.push({nx,ny});
-                }
+        visited[sr][sc]=true;
+        
+        vector<int>dX={-1,0,1,0};
+        vector<int>dY={0,1,0,-1};
+        for(int i=0;i<4;i++){
+            int nx=sr+dX[i];
+            int ny=sc+dY[i];
+            if(nx>=0&&nx<n&&ny>=0&&ny<m&&!visited[nx][ny]&&image[nx][ny]==original){
+                dfs(nx,ny,color,original,image,visited);
             }
         }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        n=image.size(),m=image[0].size();
+        vector<vector<bool>>visited(n,vector<bool>(m,false));
+        dfs(sr,sc,color,image[sr][sc],image,visited);
         return image;
     }
 };
