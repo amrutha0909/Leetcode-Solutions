1b054d2e3957f61f5edaa530a8ada127ae147455
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-07 19:35:47 +0530

    Time: 77 ms (14.07%), Space: 45.6 MB (8.08%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        queue<pair<pair<int,int>,int>>q;
-        q.push({{0,0},1});
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<bool>>visited(n,vector<bool>(m,false));
-        if(grid[0][0]!=0||grid[n-1][m-1]!=0)return -1;
-        visited[0][0]=true;
+        int n=grid.size(),m=grid[0].size();
+        if(grid[0][0]!=0 ||grid[n-1][m-1]!=0)return -1;
+        queue<pair<int,pair<int,int>>>q;
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        q.push({1,{0,0}});
+        vis[0][0]=true;
         while(!q.empty()){
             auto it=q.front();
             q.pop();
-            int x=it.first.first;
-            int y=it.first.second;
-            int steps=it.second;
-            if(x==n-1&&y==m-1)return steps;
-            vector<int>dX={-1,0,1,0,-1,1,-1,1};
-            vector<int>dY={0,1,0,-1,-1,1,1,-1};
+            int x=it.second.first;
+            int y=it.second.second;
+            int dist=it.first;
+            if(x==n-1&&y==m-1)return dist;
+            vector<int>dX={-1,0,1,0,-1,-1,1,1};
+            vector<int>dY={0,1,0,-1,1,-1,1,-1};
             for(int i=0;i<8;i++){
                 int nx=x+dX[i];
                 int ny=y+dY[i];
-                if(nx>=0&&nx<n&&ny>=0&&ny<m&&!visited[nx][ny]&&grid[nx][ny]==0){
-                    visited[nx][ny]=true;
-                    q.push({{nx,ny},steps+1});
+                if(nx>=0&&nx<n&&ny>=0&&ny<m&&!vis[nx][ny]&&grid[nx][ny]==0){
+                    vis[nx][ny]=true;
+                    q.push({dist+1,{nx,ny}});
                 }
             }
         }
