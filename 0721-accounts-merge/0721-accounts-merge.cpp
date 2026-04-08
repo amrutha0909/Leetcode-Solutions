f1c36373dd7e483b991fa3f321f1af5aa244a272
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-08 13:40:01 +0530

    Time: 22 ms (93.91%), Space: 40.5 MB (53.4%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int find(int i,vector<int>&parent){
-        if(i==parent[i])return i;
-        return parent[i]=find(parent[i],parent);
+    int find(int a, vector<int>&parent){
+        if(a==parent[a])return a;
+        return parent[a]=find(parent[a],parent);
     }
-    void unionFind(int x, int y,vector<int>&parent){
-        int xParent=find(x,parent);
-        int yParent=find(y,parent);
-        if(xParent!=yParent){
-            parent[xParent]=yParent;
+    void unionF(int x,int y,vector<int>&parent){
+        int parentX=find(x,parent);
+        int parentY=find(y,parent);
+        if(parentX!=parentY){
+            parent[parentX]=parentY;
         }
     }
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
         int n=accounts.size();
-        unordered_map<string,int>mpp;
         vector<int>parent(n);
         for(int i=0;i<n;i++){
             parent[i]=i;
         }
+        unordered_map<string,int>mpp;
         for(int i=0;i<n;i++){
             for(int j=1;j<accounts[i].size();j++){
                 string mail=accounts[i][j];
@@ -25,12 +25,12 @@ public:
                     mpp[mail]=i;
                 }
                 else{
-                    unionFind(mpp[mail],i,parent);
+                    unionF(mpp[mail],i,parent);
                 }
             }
-        }   
+        }
         vector<vector<string>>mailMerged(n);
-        for(auto&it:mpp){
+        for(auto it:mpp){
             string mail=it.first;
             int node=find(it.second,parent);
             mailMerged[node].push_back(mail);
@@ -39,10 +39,11 @@ public:
         for(int i=0;i<n;i++){
             if(mailMerged[i].size()==0)continue;
             sort(mailMerged[i].begin(),mailMerged[i].end());
+            string name=accounts[i][0];
             vector<string>temp;
-            temp.push_back(accounts[i][0]);
-            for(auto&it:mailMerged[i]){
-                temp.push_back(it);
+            temp.push_back(name);
+            for(auto x:mailMerged[i]){
+                temp.push_back(x);
             }
             result.push_back(temp);
         }
