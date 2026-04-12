b3501fa5fca2221d0d8853086d19b8e44f4313cf
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-12 11:40:06 +0530

    Time: 7 ms (24.64%), Space: 32.1 MB (21.34%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -5,10 +5,11 @@ public:
         int total=0;
         for(int i=0;i<time.size();i++){
             int rem=time[i]%60;
-            if(mpp.find((60-rem)%60)!=mpp.end()){
-                total+=mpp[(60-rem)%60];
+            int complement=(60-rem)%60;
+            if(mpp.find(complement)!=mpp.end()){
+                total+=mpp[complement];
             }
-            mpp[time[i]%60]++;
+            mpp[rem]++;
         }
         return total;
     }
