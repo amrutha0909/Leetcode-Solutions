e4dc061a0fcddf59e6eb839f57adf8e640232079
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-16 20:42:20 +0530

    Time: 1 ms (93.14%), Space: 22.7 MB (59.34%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,52 +1,69 @@
 class Solution {
 public:
-    int n,m;
-    void dfs(int r,int c,vector<vector<int>>&heights, vector<vector<bool>>&vis){
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        //make 2 queues and visited vectors for pacific and atlantic
+        //perform bfs on atlantic sides bottom row and right column. and pacific sides top row and left column
+        int n=heights.size(),m=heights[0].size();
+        vector<vector<bool>>paciVis(n,vector<bool>(m,false));
+        vector<vector<bool>>atlaVis(n,vector<bool>(m,false));
+        queue<pair<int,int>>p;
+        queue<pair<int,int>>a;
+        for(int i=0;i<n;i++){
+            paciVis[i][0]=true;
+            p.push({i,0});
+        }
+        for(int i=0;i<n;i++){
+            atlaVis[i][m-1]=true;
+            a.push({i,m-1});
+        }
+        for(int j=0;j<m;j++){
+            paciVis[0][j]=true;
+            p.push({0,j});
+        }
+        for(int j=0;j<m;j++){
+            atlaVis[n-1][j]=true;
+            a.push({n-1,j});
+        }
         vector<int>dX={-1,0,1,0};
         vector<int>dY={0,1,0,-1};
-        for(int k=0;k<4;k++){
-            int nx=r+dX[k];
-            int ny=c+dY[k];
-            if(nx>=0&&nx<n&&ny>=0&&ny<m&&heights[nx][ny]>=heights[r][c] && !vis[nx][ny]){
-                vis[nx][ny]=true;
-                dfs(nx,ny,heights,vis);
+        while(!p.empty()){
+            int len=p.size();
+            for(int k=0;k<len;k++){
+                auto it=p.front();p.pop();
+                int x=it.first;
+                int y=it.second;
+                for(int i=0;i<4;i++){
+                    int nx=x+dX[i];
+                    int ny=y+dY[i];
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && heights[nx][ny]>=heights[x][y] && !paciVis[nx][ny]){
+                        paciVis[nx][ny]=true;
+                        p.push({nx,ny});
+                    }
+                }
             }
-        }
-    }
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        n=heights.size();
-        m=heights[0].size();
-        vector<vector<int>>result;
-        vector<vector<bool>>paciVis(n,vector<bool>(m,false));
-        vector<vector<bool>>atlanVis(n,vector<bool>(m,false));
-        // queue<pair<int,int>>pq;
-        // queue<pair<int,int>>aq;
-        for(int i=0;i<n;i++){
-            //pq.push({i,0});
-            paciVis[i][0]=true;
-            dfs(i,0,heights,paciVis);
-        }
-        for(int j=0;j<m;j++){
-            //pq.push({0,j});
-            paciVis[0][j]=true;
-            dfs(0,j,heights,paciVis);
-        }
-        for(int i=0;i<n;i++){
-            // aq.push({i,m-1});
-            atlanVis[i][m-1]=true;
-            dfs(i,m-1,heights,atlanVis);
             
         }
-        for(int j=0;j<m;j++){
-            // aq.push({n-1,j});
-            atlanVis[n-1][j]=true;
-            dfs(n-1,j,heights,atlanVis);
+        while(!a.empty()){
+            int len=a.size();
+            for(int k=0;k<len;k++){
+                auto it=a.front();a.pop();
+                int x=it.first;
+                int y=it.second;
+                for(int i=0;i<4;i++){
+                    int nx=x+dX[i];
+                    int ny=y+dY[i];
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && heights[nx][ny]>=heights[x][y] && !atlaVis[nx][ny]){
+                        atlaVis[nx][ny]=true;
+                        a.push({nx,ny});
+                    }
+                }
+            }
+            
         }
-        // bfs(pq,heights,paciVis);
-        // bfs(aq,heights,atlanVis);
+        vector<vector<int>>result;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(paciVis[i][j]&&atlanVis[i][j])result.push_back({i,j});
+                if(paciVis[i][j] && atlaVis[i][j])result.push_back({i,j});
             }
         }
         return result;
