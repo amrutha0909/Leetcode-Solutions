7d4d2a81a5241ce300a9bc6bdbfa9ac614859c8f
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-19 00:32:24 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.56%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        // Find rightmost index i where arr[i] < arr[i+1]
+        // Find rightmost index j where arr[j] > arr[i]
+        // Swap arr[i] and arr[j]
+        // Reverse suffix after index i
         int n=nums.size();
         int i=n-2;
-        while(i>=0 && nums[i]>=nums[i+1]){
-            i--;
-        }
+        while(i>=0 && nums[i]>=nums[i+1])i--;
         if(i>=0){
             int j=n-1;
-            while(nums[j]<=nums[i]){
-                j--;
-            }
+            while(nums[j]<=nums[i])j--;
             swap(nums[i],nums[j]);
         }
         reverse(nums.begin()+i+1,nums.end());
-
     }
 };
