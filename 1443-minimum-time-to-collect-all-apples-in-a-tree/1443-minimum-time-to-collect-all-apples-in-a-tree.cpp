907e17bb2c73d289d4b4c15354424ed6e661b00f
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-04-18 17:53:35 +0530

    Time: 55 ms (53.24%), Space: 70 MB (40%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int dfs(int node,int parent,vector<vector<int>>&adj,vector<bool>&hasApple){
         int time=0;
-        for(auto it:adj[node]){
-            if(it==parent)continue;
-            int childTime=dfs(it,node,adj,hasApple);
-            if(childTime>0||hasApple[it]){
+        for(auto child:adj[node]){
+            if(child==parent)continue;
+            int childTime=dfs(child,node,adj,hasApple);
+            if(childTime>0 || hasApple[child]){
                 time+=childTime+2;
             }
         }
@@ -14,9 +14,8 @@ public:
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
         vector<vector<int>>adj(n);
         for(auto edge:edges){
-            int u=edge[0],v=edge[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
         }
         return dfs(0,-1,adj,hasApple);
     }
