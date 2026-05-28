289e446e9a69328d51c062c75a85f11f628594ff
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-05-28 11:10:39 +0530

    Time: 0 ms (100%), Space: 17.7 MB (58.85%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int minProduct=nums[0];
-        int maxProduct=nums[0];
-        int result=nums[0];
-        
+        int minSoFar=nums[0];
+        int maxSoFar=nums[0];
+        int ans=nums[0];
         for(int i=1;i<nums.size();i++){
-            int current=nums[i];
-            int tempMax=max({current,minProduct*current,maxProduct*current});
-            minProduct=min({current,current*minProduct,current*maxProduct});
-            maxProduct=tempMax;
-            result=max(result,maxProduct);
+            int curr=nums[i];
+            int tempMax=max(curr,max(minSoFar*curr,maxSoFar*curr));
+            minSoFar=min(curr,min(minSoFar*curr,maxSoFar*curr));
+            maxSoFar=tempMax;
+            ans=max(ans,maxSoFar);
         }
-        return result;
+        return ans;
     }
 };
