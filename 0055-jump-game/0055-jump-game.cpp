d7185b5babcc165999ac002210a918d215a347e2
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-06-23 00:25:53 +0530

    Time: 0 ms (100%), Space: 52.4 MB (26.31%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,9 +3,8 @@ public:
     bool canJump(vector<int>& nums) {
         int farthest=0;
         for(int i=0;i<nums.size();i++){
-            if(i>farthest)return false;
+            if(i>farthest)return false; //this means i have arrived at a position my previous jumps couldn't reach(crossed farthest possible jump). so it means there is no way of reaching last index.
             farthest=max(farthest,i+nums[i]);
-            
         }
         return true;
     }
