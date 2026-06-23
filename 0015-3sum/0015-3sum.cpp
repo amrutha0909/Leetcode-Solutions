2036fa610f12e03387e0f4868e31bb13f2ce688f
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-06-23 12:26:11 +0530

    Time: 45 ms (71.87%), Space: 29 MB (72.97%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
         int n=nums.size();
+        sort(nums.begin(),nums.end());
         for(int i=0;i<n-2;i++){
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            if(i>0 && nums[i]==nums[i-1])continue;
             int left=i+1,right=n-1;
             while(left<right){
-                if(nums[i]+nums[left]+nums[right]==0){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum==0){
                     ans.push_back({nums[i],nums[left],nums[right]});
                     left++;
                     right--;
-
-                    // skip duplicates for left
-                    while(left < right && nums[left] == nums[left - 1]) left++;
-
-                    // skip duplicates for right
-                    while(left < right && nums[right] == nums[right + 1]) right--;
+                    while(left<right && nums[left]==nums[left-1])left++;
+                    while(left<right && nums[right]==nums[right+1])right--;
                 }
-                else if(nums[i]+nums[left]+nums[right]<0){
-                    left++;
-                }
-                else{
-                    right--;
-                }
+                else if(sum>0)right--;
+                else left++;
+                
             }
         }
         return ans;
