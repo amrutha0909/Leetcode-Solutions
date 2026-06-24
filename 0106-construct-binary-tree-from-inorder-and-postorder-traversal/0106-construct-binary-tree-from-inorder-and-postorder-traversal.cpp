237cefd0b2b79f8c09eca80c6c37c4d7603a80bc
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-06-24 21:33:16 +0530

    Time: 7 ms (25.72%), Space: 27.7 MB (13.81%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -12,13 +12,13 @@
 class Solution {
 public:
     TreeNode*build(int inorderStart,int inorderEnd,int postorderStart,int postorderEnd,vector<int>&inorder,vector<int>&postorder,unordered_map<int,int>&mpp){
-        if(inorderStart>inorderEnd||postorderStart>postorderEnd)return nullptr;
+        if(inorderStart>inorderEnd || postorderStart>postorderEnd)return nullptr;
         int rootVal=postorder[postorderEnd];
         TreeNode*root=new TreeNode(rootVal);
-        int inorderRootIndex=mpp[rootVal];
-        int leftSubtreeSize=inorderRootIndex-inorderStart;
-        root->left=build(inorderStart,inorderRootIndex-1,postorderStart,postorderStart+leftSubtreeSize-1,inorder,postorder,mpp);
-        root->right=build(inorderRootIndex+1,inorderEnd,postorderStart+leftSubtreeSize,postorderEnd-1,inorder,postorder,mpp);
+        int inorderIndex=mpp[rootVal];
+        int leftSubtreeSize=inorderIndex-inorderStart;
+        root->left=build(inorderStart,inorderIndex-1,postorderStart,postorderStart+leftSubtreeSize-1,inorder,postorder,mpp);
+        root->right=build(inorderIndex+1,inorderEnd,postorderStart+leftSubtreeSize,postorderEnd-1,inorder,postorder,mpp);
         return root;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
