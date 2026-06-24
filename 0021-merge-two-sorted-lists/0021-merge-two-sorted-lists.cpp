0fe2ee57a667b95e5024a0399e5632e07b7da20b
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-06-24 12:47:02 +0530

    Time: 0 ms (100%), Space: 19.5 MB (27.21%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,29 +10,30 @@
  */
 class Solution {
 public:
-    struct compare{
-        bool operator()(ListNode*a,ListNode*b){
-            return a->val>b->val;
-        }
-    };
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        priority_queue<ListNode*,vector<ListNode*>,compare>pq;
-        if(!list1&&!list2)return nullptr;
-        if(!list1)return list2;
-        if(!list2)return list1;
-        pq.push(list1);
-        pq.push(list2);
         ListNode*dummy=new ListNode(-1);
         ListNode*temp=dummy;
-        while(!pq.empty()){
-            ListNode*curr=pq.top();
-            pq.pop();
-            temp->next=curr;
-            temp=temp->next;
-            if(curr->next){
-                pq.push(curr->next);
-                curr=curr->next;
+        while(list1&&list2){
+            if(list1->val<=list2->val){
+                temp->next=list1;
+                list1=list1->next;
             }
+            else{
+                temp->next=list2;
+                list2=list2->next;
+                
+            }
+            temp=temp->next;
+        }
+        while(list1){
+            temp->next=list1;
+            list1=list1->next;
+            temp=temp->next;
+        }
+        while(list2){
+            temp->next=list2;
+            list2=list2->next;
+            temp=temp->next;
         }
         return dummy->next;
     }
