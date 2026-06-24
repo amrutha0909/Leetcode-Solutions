f126333cdfac7168b734877d6ffeef461986c47a
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-06-24 12:24:54 +0530

    Time: 0 ms (100%), Space: 11.2 MB (56.03%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        vector<string>stack;
+        vector<string>stk;
         string token;
         stringstream ss(path);
         while(getline(ss,token,'/')){
             if(token==""||token==".")continue;
             if(token==".."){
-                if(!stack.empty()){
-                    stack.pop_back();
-                }
+                if(!stk.empty())stk.pop_back();
             }
             else{
-                stack.push_back(token);
+                stk.push_back(token);
             }
         }
         string ans="";
-        for(string dir:stack){
+        for(string dir:stk){
             ans+="/"+dir;
         }
         return ans.empty()?"/":ans;
