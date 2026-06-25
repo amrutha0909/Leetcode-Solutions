a2234d79dddc52a6173cb3f28d44925025db81f9
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-06-25 21:41:26 +0530

    Time: 0 ms (100%), Space: 14.3 MB (69.41%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -6,7 +6,6 @@ public:
         int n=board.size();
         int m=board[0].size();
         visited[x][y]=true;
-        board[x][y]='X';
         for(int i=0;i<4;i++){
             int nx=x+dX[i];
             int ny=y+dY[i];
@@ -20,21 +19,16 @@ public:
         int n=board.size();
         vector<vector<bool>>visited(n,vector<bool>(m,false));
         for(int i=0;i<n;i++){
-            visited[i][0]=true;
+            if(board[i][0]=='O')dfs(i,0,board,visited);
+            if(board[i][m-1]=='O')dfs(i,m-1,board,visited);
         }
         for(int i=0;i<m;i++){
-            visited[0][i]=true;
+            if(board[0][i]=='O')dfs(0,i,board,visited);
+            if(board[n-1][i]=='O')dfs(n-1,i,board,visited);
         }
-        for(int i=0;i<n;i++){
-            visited[i][m-1]=true;
-        }
-        for(int i=0;i<m;i++){
-            visited[n-1][i]=true;
-        }
-        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!visited[i][j] && board[i][j]=='O')dfs(i,j,board,visited);
+                if(!visited[i][j] && board[i][j]=='O')board[i][j]='X';
             }
         }
     }
