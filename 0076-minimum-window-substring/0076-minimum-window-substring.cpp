b35502f1768f30fe12fe79a42bfe60f743c00be7
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-07-01 23:50:07 +0530

    Time: 13 ms (39.89%), Space: 11.6 MB (43.29%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<int,int>mpp;
-        for(char c:t){
-            mpp[c]++;
-        }
-        int target=t.size();
+        vector<int>window{0,INT_MAX};
         int left=0;
-        vector<int>minWindow={0,INT_MAX};
+        unordered_map<int,int>mpp;
+        for(char c:t)mpp[c]++;
+        int target=t.size();
         for(int right=0;right<s.size();right++){
             if(mpp.find(s[right])!=mpp.end() && mpp[s[right]]>0){
                 target--;
@@ -15,22 +13,22 @@ public:
             mpp[s[right]]--;
             if(target==0){
                 while(true){
-                    if(mpp.find(s[left])!=mpp.end()&&mpp[s[left]]==0){
+                    if(mpp.find(s[left])!=mpp.end() && mpp[s[left]]==0){
                         break;
                     }
                     mpp[s[left]]++;
                     left++;
                 }
-                if(right-left<minWindow[1]-minWindow[0]){
-                    minWindow[0]=left;
-                    minWindow[1]=right;
+                if(right-left<window[1]-window[0]){
+                    window[1]=right;
+                    window[0]=left;
                 }
                 mpp[s[left]]++;
                 left++;
                 target++;
-            } 
+            }
         }
-        if(minWindow[1]>s.size())return "";
-        return s.substr(minWindow[0],minWindow[1]-minWindow[0]+1);
+        if(window[1]>s.size())return "";
+        return s.substr(window[0],window[1]-window[0]+1);
     }
 };
