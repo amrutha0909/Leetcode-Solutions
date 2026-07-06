614aa2723eb3823e639bf39111635d13fd797d70
Amrutha Maddipati <139883552+amrutha0909@users.noreply.github.com>
2026-07-06 11:09:50 +0530

    Time: 0 ms (100%), Space: 10.7 MB (23.11%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    // int helper(int index,vector<int>&nums,vector<int>&dp){
-    //     if(index>=nums.size())return 0;
-    //     if(dp[index]!=-1)return dp[index];
-    //     int not_take=helper(index+1,nums,dp);
-    //     int take=nums[index]+helper(index+2,nums,dp);
-    //     return dp[index]=max(take,not_take);
-    // }
+    int helper(int i,vector<int>&nums,vector<int>&dp){
+        if(i>=nums.size())return 0;
+        if(dp[i]!=-1)return dp[i];
+        int not_take=helper(i+1,nums,dp);
+        int take=nums[i]+helper(i+2,nums,dp);
+        return dp[i]=max(take,not_take);
+    }
     int rob(vector<int>& nums) {
-        int n=nums.size()-1;
-        vector<int>dp(n+1,-1);
-        //return helper(0,nums,dp);
-        if(nums.size()==1)return nums[0];
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<=n;i++){
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
-        }
-        return dp[n];
+        vector<int>dp(nums.size(),-1);
+        return helper(0,nums,dp);
     }
 };
